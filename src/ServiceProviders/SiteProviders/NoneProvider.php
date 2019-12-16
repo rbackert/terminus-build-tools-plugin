5acafe4093511f1d9d56d3a609d4d4d970d6d082
Rachel Backert <rbackert@mindgrub.com>
2019-12-16 09:19:24 -0500

    implement infer method

--- src/ServiceProviders/SiteProviders/NoneProvider.php
+++ src/ServiceProviders/SiteProviders/NoneProvider.php
@@ -18,5 +18,14 @@ class NoneProvider implements SiteProvider {
 	public function setMachineToken( $token ) {
         $this->machineToken = '';
         return $this;
+	}
+	
+	/**
+     * Used to infer the provider to create from an identifier. Return
+     * 'true' if this provider identifies with the URL (e.g. GithubProvider
+     * expects the url to be for github.com).
+     */
+    public function infer( $url ) {
+        return empty( $url );
     }
 }
