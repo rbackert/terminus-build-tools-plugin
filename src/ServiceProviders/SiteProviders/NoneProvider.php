ba461915b0d3fc003ae0289c238ba5bf8968fd59
Rachel Backert <rbackert@mindgrub.com>
2019-12-16 09:22:55 -0500

    implement setSession

--- src/ServiceProviders/SiteProviders/NoneProvider.php
+++ src/ServiceProviders/SiteProviders/NoneProvider.php
@@ -8,24 +8,42 @@ class NoneProvider implements SiteProvider {
 	}
 
 	public function getEnvironment() {
-		if ( !$this->siteEnvironment ) {
+		if ( ! $this->siteEnvironment ) {
 			$this->siteEnvironment = ( new SiteEnvironment() )
 				->setServiceName( $this->getServiceName() );
 		}
 		return $this->siteEnvironment;
 	}
 
+	/**
+	 * Set the machine token to access the site. Blank in this case.
+	 *
+	 * @param string $token
+	 * @return NoneProvider
+	 */
 	public function setMachineToken( $token ) {
-        $this->machineToken = '';
-        return $this;
+		$this->machineToken = '';
+		return $this;
 	}
 	
 	/**
-     * Used to infer the provider to create from an identifier. Return
-     * 'true' if this provider identifies with the URL (e.g. GithubProvider
-     * expects the url to be for github.com).
-     */
-    public function infer( $url ) {
-        return empty( $url );
-    }
+	 * Used to infer the provider to create from an identifier. Return
+	 * 'true' if this provider identifies with the URL (e.g. GithubProvider
+	 * expects the url to be for github.com).
+	 *
+	 * @param string $url
+	 * @return boolean
+	 */
+	public function infer( $url ) {
+		return empty( $url );
+	}
+	
+	public function session() {
+		return $this->session;
+	}
+
+	public function setSession( $session ) {
+		$this->session = $session;
+		return $this;
+	}
 }
